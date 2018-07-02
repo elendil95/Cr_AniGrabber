96e6bc3a9fd750ecec03c7759fbd470428732e52
Unknown <uge1995@gmail.com>
2018-07-02 15:54:45 +0200

    added season option, input file broke

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import subprocess
 
 PATH = "~/CR Anime/"
 series_name = input("Input name of the anime (Romanji only): ")
+season_nr = input("Input a season number (The season you want, not the total number of seasons):") 
 full_path=PATH+series_name
 normal_path=os.path.expanduser(full_path)
 
@@ -13,10 +14,19 @@ if (os.path.isdir(normal_path)):
 else:
 	os.makedirs(normal_path)
 	print("Creating directory "+series_name+" in "+PATH)
-	
-input_file = input("Please select a file (please include extnsion if present): ")			
 
-if os.path.isfile(os.path.join(normal_path, input_file)) is True:
+seasons=os.listdir(normal_path)
+if seasons == []:
+	print("There are currently no seasons of this anime: creating season ", season_nr,"...")
+	os.makedirs(os.path.join(normal_path, "Season %s" %(season_nr)))
+else:		
+	print("Anime \'"+series_name+"\'"+" contains the following seasons already: "+str(seasons))
+	if season_nr in seasons:
+		print("this season already exisits")
+		sys.exit(1)
+
+input_file = input("Please select a file (please include extnsion if present): ")			
+if os.path.isfile(os.path.join(PATH, input_file)) is True:
 	print("File "+input_file+" found.")
 else: 
 	print("File "+input_file+" does not exist")
