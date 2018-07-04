bfcaf04f06be0a4320b79ac669065d06cc58724c
Unknown <uge1995@gmail.com>
2018-07-04 10:14:19 +0200

    minor changes for readability

--- main.py
+++ main.py
@@ -8,30 +8,33 @@ import subprocess
 
 PATH = "~/CR Anime/"
 series_name = input("Input name of the anime (Romanji only): ")
-season_nr = input("Input a season number (The season you want, not the total number of seasons):")
+
 episode_nr = None 
 #full_path=PATH+series_name
 normal_path=os.path.expanduser(os.path.join(PATH, series_name))
 
 if (os.path.isdir(normal_path)):
-	print("Folder "+series_name+" already exists.")
+	print("Series \'"+series_name+"\'"+" already exists, moving into it...")
 else:
 	os.makedirs(normal_path)
-	print("Creating directory "+series_name+" in "+PATH)
+	print("Creating series \'"+series_name+"\'"+" in "+PATH)
 
+season_nr = input("Input a season number (The season you want, not the total number of seasons):")
 seasons=os.listdir(normal_path)
 if seasons == []:
-	print("There are currently no seasons of this anime: creating season ", season_nr,"...")
+	print("There are currently no seasons of this anime: creating season ",season_nr,"...")
 	os.makedirs(os.path.join(normal_path, "Season %s" %(season_nr)))
-else:		
-	print("Anime \'"+series_name+"\'"+" contains the following seasons already: "+str(seasons))
+else:
+	season_str=', '.join(sorted(seasons))		
+	print("Anime \'"+series_name+"\'"+" contains the following seasons already: "+season_str+'\n')
 	if str("Season %s" %(season_nr)) in seasons:
-		print("this season already exisits")
+		print("this season already exisits! Please re-run the program and select a valid season number")
 		sys.exit(1)
 	else:
+		print("Season number is valid, creating season "+season_nr+"..."+'\n')
 		os.makedirs(os.path.join(normal_path, "Season %s" %(season_nr)))	 	
 
-input_file = input("Please select a file (please include extnsion if present): ")
+input_file = input("Please select an input file containing episode urls. (include extnsion if present): ")
 input_file=os.path.expanduser(os.path.join(PATH, input_file))			
 if os.path.isfile(input_file) is True:
 	print("File "+input_file+" found.")
@@ -40,19 +43,19 @@ if os.path.isfile(input_file) is True:
 else:
 	file=str(os.path.join(PATH, input_file))
 	print(file)
-	print("File "+input_file+" does not exist")
+	print("File "+input_file+" does not exist. re-run the program and select a valid file")
 	sys.exit(1)
-
+print("done"+'\n')
 print("Creating last directory structure...")
 os.chdir(os.path.expanduser(os.path.join(normal_path, "Season %s" %(season_nr))))
 for x in range(episode_nr):
 	os.makedirs("Episode %d" %(x+1))
 
-ydl_opts = {
-	'verbose': True,
-}
+# ydl_opts = {
+# 	'verbose': True,
+# }
 
-with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-    meta = ydl.extract_info(
-        'http://www.crunchyroll.com/konosuba-gods-blessing-on-this-wonderful-world/episode-1-this-self-proclaimed-goddess-and-reincarnation-in-another-world-692393', download=False)
-print(meta)
+# with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+#     meta = ydl.extract_info(
+#         'http://www.crunchyroll.com/konosuba-gods-blessing-on-this-wonderful-world/episode-1-this-self-proclaimed-goddess-and-reincarnation-in-another-world-692393', download=False)
+# print(meta)
