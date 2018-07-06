45cceee7d3adefcbe12b8a4080ed00eddb2dc035
Unknown <uge1995@gmail.com>
2018-07-06 17:47:09 +0200

    not much progress, but working with info_dict should yeld good results.

--- yt_test.py
+++ yt_test.py
@@ -6,21 +6,29 @@ import argparse
 import subprocess
 
 ydl_opts = {
-	#'verbose': True,
-	'dump_single_json': True,
+	'verbose': True,
+	#'simulate': True,
+	#'dump_single_json': True,
 	#'batchfile': 
 	#'format': "720p-1",
 	#'autonumber_size': 1
 	#'outtmpl':
-	'writethumbnail': True,
-	'subtitlesformat': "srt",
-	'subtitleslangs': "enUS",
+	#'writethumbnail': True,
+	#'embedthumbnail': True,
+	#'listsubtitles': True,
 	'writesubtitles': True,
+	'subtitlesformat': "srt",
+	'allsubtitles': True,
+	#'subtitleslangs': "en",
 	'prefer_ffmpeg': True,
 	'embedsubtitles': True,
-	'embedthumbnail': True
 	#'postprocessor_args'
 }
 
 with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-    ydl.download(['http://www.crunchyroll.com/is-the-order-a-rabbit/episode-1-i-knew-at-first-glance-that-it-was-no-ordinary-fluffball-653243'])
+	 info_dict = ydl.extract_info("http://www.crunchyroll.com/is-the-order-a-rabbit/episode-1-i-knew-at-first-glance-that-it-was-no-ordinary-fluffball-653243", download=False)
+	 print(info_dict.re)
+	 ##SUbtitle information and all the other stuff can be extracted from the dictionary given the chanche
+	  
+	#ydl.list_subtitles("http://www.crunchyroll.com/is-the-order-a-rabbit/episode-1-i-knew-at-first-glance-that-it-was-no-ordinary-fluffball-653243")
+   #ydl.download(['http://www.crunchyroll.com/is-the-order-a-rabbit/episode-1-i-knew-at-first-glance-that-it-was-no-ordinary-fluffball-653243'])
